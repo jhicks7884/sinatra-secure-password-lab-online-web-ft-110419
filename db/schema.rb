e792bd6a8d984d8b487671260ef3b9731dbece2b
Jeshua Hicks <jhicks7884@outlook.com>
2020-01-14 19:05:00 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20200114185020) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
+    t.string "password_digest"
   end
 
 end
